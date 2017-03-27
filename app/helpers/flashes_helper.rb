ce02603c9916779e3d71ae701c8e3400c56b1b75
zsmhy <zsm@example.com>
2017-03-28 07:44:39 +0800

    add bootstrap flash function

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -1,6 +1,6 @@
 module FlashesHelper
   FLASH_CLASSES = { alert: "danger", notice: "success", warning: "warning"}.freeze
-  def falsh_class(key)
+  def flash_class(key)
      FLASH_CLASSES.fetch key.to_sym, key
   end
 
